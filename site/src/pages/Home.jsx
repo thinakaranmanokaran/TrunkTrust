60e526503e6541529887d159dd8c5679457ff256
Thinakaran <thinakaranmanohar@yahoo.com>
2025-09-18 20:59:12 +0530

    Update .gitignore and clean up dist assets
    Added 'site/dist/' to .gitignore to prevent tracking of build artifacts. Removed generated JS and CSS files from site/dist/assets and updated site/dist/index.html and site/src/pages/Home.jsx. package-lock.json was also updated.

--- site/src/pages/Home.jsx
+++ site/src/pages/Home.jsx
@@ -3,6 +3,7 @@ import images from '../assets/images'
 import { AnimatePresence, motion } from "framer-motion";
 import { FiPlus, FiMinus } from "react-icons/fi";
 import { HiHeart } from "react-icons/hi2";
+import { GoPlus } from "react-icons/go";
 
 const Home = () => {
 
@@ -61,10 +62,10 @@ const Home = () => {
         <div className='font-dmsans min-h-screen'>
             <div>
                 {/* Navbar */}
-                <div className='h-24 shadow-md md:flex items-center px-12 justify-between'>
+                <div className='xl:h-20 2xl:h-24 shadow-md p-2 md:flex items-center md:px-12 justify-between'>
                     <div className='flex items-center space-x-2'>
-                        <img src={images.Logo2} className='w-16 h-16 rounded-xl' alt="Trunk Trust Logo" />
-                        <h1 className='font-bold text-4xl tracking-tighter text-dark/90'>Trunk Trust</h1>
+                        <img src={images.Logo2} className='w-12 h-12 md:w-16 md:h-16 rounded-xl' alt="Trunk Trust Logo" />
+                        <h1 className='font-bold text-2xl md:text-4xl tracking-tighter text-dark/90'>Trunk Trust</h1>
                     </div>
                     <div className='text-xl hidden md:flex space-x-3 tracking-tighter'>
                         <a
@@ -87,10 +88,10 @@ const Home = () => {
                 </div>
 
                 {/* Hero Section */}
-                <div className='w-full flex flex-col items-center py-12'>
-                    <img src={images.LogoTR} className='w-60 h-60' alt="Trunk Trust Icon" />
+                <div className='w-full flex flex-col items-center xl:py-6 2xl:py-12'>
+                    <img src={images.LogoTR} className='w-40 md:w-60 md:h-60' alt="Trunk Trust Icon" />
 
-                    <h1 className='text-6xl font-bold tracking-tighter w-2/4 text-center leading-tight'>
+                    <h1 className='text-3xl md:text-6xl font-bold tracking-tighter xl:w-3/4 2xl:w-2/4 text-center leading-tight'>
                         The No.1 Lightweight API Testing Software on the Internet.
                     </h1>
 
@@ -98,17 +99,17 @@ const Home = () => {
                     <a
                         href="/TrunkTrust-win_x64_Packed.exe"
                         download
-                        className='my-2 mt-8 bg-[#2a4fec] px-8 min-w-60 min-h-18 cursor-pointer 
+                        className='my-2 mt-4 md:mt-8 bg-[#2a4fec] px-6 py-3 md:px-8 md:min-w-60 md:min-h-18 cursor-pointer 
                                    hover:brightness-110 transition-all duration-300 
                                    hover:scale-105 active:scale-95 flex items-center justify-center rounded-full 
-                                   text-xl text-white text-center'
+                                   md:text-xl text-white text-center'
                     >
                         Download
                     </a>
 
-                    <p className='text-dark/70 tracking-tight'>For Windows 11/10 (64-bit)</p>
+                    <p className='text-dark/70 tracking-tight text-sm md:text-base'>For Windows 11/10 (64-bit)</p>
 
-                    <p className='text-dark/70 tracking-tight text-lg mt-4'>
+                    <p className='text-dark/70 tracking-tight text-center md:text-lg mt-4'>
                         Download other versions for macOS and Linux&nbsp;
                         <a
                             href="https://github.com/thinakaranmanokaran/TrunkTrust/tree/main/dist/TrunkTrust"
@@ -118,8 +119,8 @@ const Home = () => {
                         </a>.
                     </p>
                 </div>
-                <div className='flex flex-col items-center'>
-                    <div className='bg-gray p-4 w-fit rounded-3xl'>
+                <div className='flex flex-col items-center mt-3 md:mt-0'>
+                    <div className='bg-gray p-2 md:p-4 w-4/5 md:w-fit rounded-3xl'>
                         <motion.img
                             src={images.Screenshot}
                             alt="Screenshot"
@@ -131,33 +132,33 @@ const Home = () => {
                         />
                     </div>
                 </div>
-                <div className='flex flex-col  items-center my-12 '>
-                    <h1 className='text-6xl font-bold tracking-tighter w-3/5 text-center  leading-tight '>Supercharge your Development Speed with Trunk Trust built right in</h1>
-                    <div className="my-12 flex w-4/5 space-x-6">
+                <div className='flex flex-col  items-center my-4 md:my-12 '>
+                    <h1 className='text-3xl md:text-6xl font-bold tracking-tighter md:w-3/5 text-center  leading-tight '>Supercharge your Development Speed with Trunk Trust built right in</h1>
+                    <div className="my-12 flex flex-col items-center md:flex-row md:w-4/5 md:space-x-6">
                         {cards.map((card, index) => (
                             <div
                                 key={index}
-                                className="shadow-md p-4 pb-12 w-1/3 rounded-3xl flex flex-col items-center"
+                                className="shadow-md p-2 pb-6 md:p-4 md:pb-12 md:w-1/3 rounded-3xl flex flex-col items-center w-80"
                             >
                                 <img src={card.img} className="rounded-2xl" alt={card.title} />
-                                <h2 className="text-3xl font-semibold tracking-tighter text-center">
+                                <h2 className="text-2xl md:text-3xl font-semibold tracking-tighter text-center">
                                     {card.title}
                                 </h2>
-                                <h2 className="text-lg text-dark/70 mt-3 w-4/5 tracking-tighter text-center">
+                                <h2 className="md:text-lg text-dark/70 mt-1 md:mt-3 md:w-4/5 tracking-tighter text-center">
                                     {card.desc}
                                 </h2>
                             </div>
                         ))}
                     </div>
                 </div>
-                <div className='flex flex-col items-center my-12'>
-                    <h1 className='text-6xl font-bold tracking-tighter w-3/5 text-center  leading-tight '>Frequently asked questions</h1>
-                    <div className="my-16 w-full max-w-2/5 mx-auto">
+                <div className='flex flex-col items-center my-4 md:my-12'>
+                    <h1 className='text-3xl md:text-6xl font-bold tracking-tighter md:w-3/5 text-center  leading-tight '>Frequently asked questions</h1>
+                    <div className="my-2 md:my-16 w-full px-3 md:px-0 md:max-w-2/5 mx-auto">
                         {faqs.map((faq, i) => (
                             <div key={i} className="border-b-2 border-b-dark/40 py-6 pb-3 ">
                                 {/* Header */}
                                 <div
-                                    className="flex text-2xl justify-between items-center cursor-pointer"
+                                    className="flex text-lg md:text-2xl justify-between items-center cursor-pointer"
                                     onClick={() => toggleFAQ(i)}
                                 >
                                     <h3 className=" font-medium tracking-tight">{faq.q}</h3>
@@ -175,7 +176,7 @@ const Home = () => {
                                             transition={{ duration: 0.3, ease: "easeInOut" }}
                                             className="overflow-hidden"
                                         >
-                                            <p className="mt-3 text-dark/80 text-xl">{faq.a}</p>
+                                            <p className="mt-3 text-dark/80 md:text-xl">{faq.a}</p>
                                         </motion.div>
                                     )}
                                 </AnimatePresence>
@@ -185,7 +186,7 @@ const Home = () => {
                 </div>
             </div>
             <div className='bg-dark h-20 flex justify-center items-center '>
-                <h1 className='text-xl tracking-tighter text-white flex  items-center'><span className="text-light/60  mr-1"> Developed by</span> <a href="https://www.thinakaran.dev/" target="_blank" rel="noopener noreferrer">Thinakaran Manokaran</a><HiHeart className='ml-1 ' /> </h1>
+                <h1 className='md:text-xl tracking-tighter text-white flex  items-center'><span className="text-light/60  mr-1"> Developed by</span> <a href="https://www.thinakaran.dev/" target="_blank" rel="noopener noreferrer">Thinakaran Manokaran</a><HiHeart className='ml-1 ' /> </h1>
             </div>
         </div>
     )
